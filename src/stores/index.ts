a3c84556dca89ee3d304d4b11af3200403c8d23d
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-09-15 15:15:09 +0200

    refactor: move the stores and utils folder to src

---
+++ src/stores/index.ts
@@ -0,0 +1,2 @@
+export { default as useLocationStore } from "./useLocationStore.ts";
+export { default as useQueryStore } from "./useQueryStore.ts";
--- stores/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as useLocationStore } from "./useLocationStore";
-export { default as useQueryStore } from "./useQueryStore";
