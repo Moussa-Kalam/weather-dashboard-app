8b7c813824a0666417b0d508f8349405bc0ac620
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-09-15 20:31:36 +0200

    refactor: remove border

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -5,7 +5,7 @@ const WeatherDisplay = () => {
   return (
     <section className="w-[80%] mx-auto space-y-16">
       <WeatherOverview />
-      <section className="flex flex-wrap gap-6 justify-center items-center border">
+      <section className="flex flex-wrap gap-6 justify-center items-center">
         <HumidityOverview />
         <CitiesOverview />
       </section>
